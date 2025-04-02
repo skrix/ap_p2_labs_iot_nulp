d0be3149a0dbd7b529cc29632a726ce4241ee4e9
Andrew <andrew.shyika@gmail.com>
2025-04-03 01:22:10 +0300

    add lab 2 level 1

---
+++ tests/lab2/test_level2.py
@@ -0,0 +1,15 @@
+import unittest
+from lab2.level2 import solution
+
+class TestLevel2(unittest.TestCase):
+
+    def test_solution(self):
+        self.assertEqual(solution([3,6,7,11], 8), 4)
+        self.assertEqual(solution([30,11,23,4,20], 5), 30)
+        self.assertEqual(solution([30,11,23,4,20], 6), 23)
+
+    def test_input_limitations(self):
+        self.assertEqual(solution([3,6,7,11], 2), None)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/lab2/test_level3.py
@@ -0,0 +1,10 @@
+import unittest
+from lab2.level3 import solution
+
+class TestLevel3(unittest.TestCase):
+
+    def test_solution(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
