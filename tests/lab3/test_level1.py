e3c10286e4f037d420476332b46d1596ec32f697
Andrew <andrew.shyika@gmail.com>
2025-04-04 00:13:09 +0300

    add lab3

---
+++ tests/lab3/test_level1.py
@@ -0,0 +1,16 @@
+import unittest
+from lab3.level1 import solution
+
+class TestLevel1(unittest.TestCase):
+
+    def test_case_1(self):
+        pass
+
+    def test_case_2(self):
+        pass
+
+    def test_case_3(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/lab3/test_level2.py
@@ -0,0 +1,10 @@
+import unittest
+from lab3.level2 import solution
+
+class TestLevel2(unittest.TestCase):
+
+    def test_solution(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
