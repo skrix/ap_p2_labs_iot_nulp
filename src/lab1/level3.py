7bc5a94293644f34f5ba5e0a83c91ef10663fe41
Andrew <andrew.shyika@gmail.com>
2025-04-05 14:36:31 +0300

    Fix pycodestyle pep

--- src/lab1/level3.py
+++ src/lab1/level3.py
@@ -1,28 +1,25 @@
 def solution(numbers):
-    max_sequence_length = 0
-    last_index = len(numbers) - 1
+    max_length = 0
+    last = len(numbers) - 1
     index = 1
-    while index < last_index:
+    while index < last:
         if (numbers[index - 1] < numbers[index]) and (
             numbers[index] > numbers[index + 1]
         ):
-            left_index = index - 1
-            right_index = index + 1
-            current_sequence_length = 3
+            left = index - 1
+            right = index + 1
+            current_length = 3
 
-            while left_index > 0 and numbers[left_index] > numbers[left_index - 1]:
-                left_index -= 1
-                current_sequence_length += 1
+            while left > 0 and numbers[left] > numbers[left - 1]:
+                left -= 1
+                current_length += 1
 
-            while (
-                right_index < last_index
-                and numbers[right_index] > numbers[right_index + 1]
-            ):
-                right_index += 1
-                current_sequence_length += 1
+            while right < last and numbers[right] > numbers[right + 1]:
+                right += 1
+                current_length += 1
 
-            max_sequence_length = max(current_sequence_length, max_sequence_length)
+            max_length = max(current_length, max_length)
 
         index += 1
 
-    return max_sequence_length
+    return max_length
