b62b7dbeaad0745ee8c41b9171be6b69f8f54e67
Andrew <andrew.shyika@gmail.com>
2025-04-28 02:59:54 +0300

    add lab3 level3

--- src/lab3/level3.py
+++ src/lab3/level3.py
@@ -1,2 +1,18 @@
-def solution():
-    pass
+from src.lab3.node import Node
+
+def binary_tree_diameter(root: Node):
+    result = 0
+
+    def diameter(node: Node):
+        nonlocal result
+
+        if not node:
+            return 0
+
+        left_height = diameter(node.left)
+        right_height = diameter(node.right)
+        result = max(result, left_height + right_height)
+        return 1 + max(left_height, right_height)
+
+    diameter(root)
+    return result
--- tests/lab3/test_level3.py
+++ tests/lab3/test_level3.py
@@ -1,11 +1,23 @@
 import unittest
-from src.lab3.level3 import solution
+from src.lab3.node import Node
+from src.lab3.level3 import binary_tree_diameter
 
 
 class TestLevel3(unittest.TestCase):
 
-    def test_solution(self):
-        pass
+    def setUp(self):
+        self.node_9 = Node(9)
+        self.node_8 = Node(8, left=self.node_9)
+        self.node_7 = Node(7, left=self.node_8)
+        self.node_6 = Node(6)
+        self.node_5 = Node(5, right=self.node_6)
+        self.node_4 = Node(4, right=self.node_5)
+        self.node_3 = Node(3, left=self.node_7, right=self.node_4)
+        self.node_2 = Node(2)
+        self.node_1 = Node(1, left=self.node_3, right=self.node_2)
+
+    def test_binary_tree_diameter(self):
+        self.assertEqual(binary_tree_diameter(self.node_1), 6)
 
 
 if __name__ == "__main__":
