b10a340e57bdc7a72e21fc2e53b3de05641054c3
Andrew <andrew.shyika@gmail.com>
2025-04-28 02:27:20 +0300

    add lab3 level2

--- src/lab3/level2.py
+++ src/lab3/level2.py
@@ -1,2 +1,13 @@
-def solution():
-    pass
+from src.lab3.node import Node
+from copy import deepcopy
+
+def invert_binary_tree(root: Node, result=None):
+    if result is None:
+       result = deepcopy(root)
+
+    if root is None:
+        return result
+
+    result.left = invert_binary_tree(root.right)
+    result.right = invert_binary_tree(root.left)
+    return result
--- tests/lab3/test_level2.py
+++ tests/lab3/test_level2.py
@@ -1,11 +1,29 @@
 import unittest
-from src.lab3.level2 import solution
+from src.lab3.node import Node
+from src.lab3.level2 import invert_binary_tree
 
 
 class TestLevel2(unittest.TestCase):
 
-    def test_solution(self):
-        pass
+    def setUp(self):
+        self.node_7 = Node(7)
+        self.node_6 = Node(6)
+        self.node_5 = Node(5)
+        self.node_4 = Node(4)
+        self.node_3 = Node(3, left=self.node_6, right=self.node_7)
+        self.node_2 = Node(2, left=self.node_4, right=self.node_5)
+        self.node_1 = Node(1, left=self.node_2, right=self.node_3)
+
+    def test_invert_binary_tree(self):
+        invered_tree = invert_binary_tree(self.node_1)
+
+        self.assertEqual(invered_tree.value, self.node_1.value)
+        self.assertEqual(invered_tree.left.value, self.node_3.value)
+        self.assertEqual(invered_tree.left.left.value, self.node_3.right.value)
+        self.assertEqual(invered_tree.left.right.value, self.node_3.left.value)
+        self.assertEqual(invered_tree.right.value, self.node_2.value)
+        self.assertEqual(invered_tree.right.left.value, self.node_2.right.value)
+        self.assertEqual(invered_tree.right.right.value, self.node_2.left.value)
 
 
 if __name__ == "__main__":
